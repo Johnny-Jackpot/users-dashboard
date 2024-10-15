7dc83ff4deae77b5e58eb8d3e00bd904e5c52190
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-10-15 15:40:39 +0300

    added login logic

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,13 @@ import {
 } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
+import axios from "axios";
 import './index.css'
+
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.headers.common['Accept'] = 'application/json';
 
 const router = createBrowserRouter([
   {
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
+import axios from "axios";
+import {useState} from "react";
 
 const LoginPage: React.FC = () => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const login = formData.get('login');
@@ -9,6 +13,20 @@ const LoginPage: React.FC = () => {
     if (!login || !password) {
       alert('Provide login and password');
       return;
+    }
+
+    try {
+      setLoading(true);
+      await axios.get('/sanctum/csrf-cookie');
+      const response = await axios.post('/api/login', {
+        email: login,
+        password,
+      });
+
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -27,8 +45,8 @@ const LoginPage: React.FC = () => {
           placeholder="Password"
           className="w-full border py-2 px-4 mb-2 rounded"
         />
-        <button type="submit" className="border py-2 px-4 mb-2 rounded">
-          Login
+        <button type="submit" className="border py-2 px-4 mb-2 rounded" disabled={loading}>
+          {loading ? 'Loading...' : 'Login'}
         </button>
       </form>
     </div>
